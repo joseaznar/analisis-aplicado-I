821fe9194de713c645f930b65232a58f2d1843c3
mate <mate@itam.mx>
2018-02-01 13:50:38 -0600

    Add the machine's epsilon to the plot and change the previous one to show the inverse of the conditional

--- Busqueda de Linea (Lab 3)/hilbert.m
+++ Busqueda de Linea (Lab 3)/hilbert.m
@@ -1,12 +1,13 @@
 % hilbert
+close all;
 w = [];
 for n = 2:20
     A = hilb(n);
     w = [w cond(A)];
 end
 
-u = [2:20];
-semilogy(u, w, 'dr', u, w, 'b--', 'Linewidth', 3);
+u = [2:20]; z = eps*ones(1, 19);
+semilogy(u, 1./w, 'dr', u, 1./w, 'b--', u, z, 'r', 'Linewidth', 3);
 title('Condicional de las matrices de Hilbert', 'Fontsize', 16)
 xlabel('Dimensión de la matriz', 'Fontsize', 16);
 ylabel('Condicional de la matriz', 'Fontsize', 16);
